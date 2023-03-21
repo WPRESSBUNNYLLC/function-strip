c9c25b57bb1883de0fa8d19fef2411232d528601
Alexander Eatman <johnathanae1995@gmail.com>
2023-03-21 13:28:44 -0400

    'update'

--- backtracking/arrow.js
+++ backtracking/arrow.js
@@ -1,4 +1,52 @@
-//named and unamed arrow functions... in and out of template literal
+/*
+ * @param {bt_arrow_parameter_string} the parameters of the arrow function. built via an array, and joined as a string returned as the beginning of the function
+ * @param {bt_index} the back tracking of an index
+ * @param {bt_index_drop_off_function_name} the index that backtracks the arrow function name. definition used for ending
+ * @param {bt_index_drop_off_alphabet} once the first character is hit, set below to on
+ * @param {bt_index_drop_off_found_first_character} once on, when first space or new line hit, end and return the name and function parameters
+ * @param {bt_index_drop_off_append_equals} once a character is found, make sure to append the equals sign before the function name
+ * @param {in_bt_quotation_string} in and out of a string within the parameter set
+ * @param {in_bt_string} compliment of above. denotes in and out of a string within the parameter set
+ * @param {opening_bt_parentheses} opening parentheses used for ending. could use count
+ * @param {closing_bt_parentheses} closing parentheses used for ending. could use count.
+ * @param {bt_af_is_async_check} used to turn on or off the condition that appends 'async'
+*/
+
+var data = '';
+var data_index = 0; //get rid of this
+var bt_arrow_parameter_string = [];
+var bt_index = 0;
+var in_bt_quotation_string = [];
+var in_bt_string = false;
+var opening_bt_parentheses = 0;
+var closing_bt_parentheses = 0;
+var is_arrow = true;
+var bt_index_drop_off_found_first_character = false;
+var bt_index_drop_off_function_name = 0;
+var bt_index_drop_off_append_equals = false;
+var bt_af_is_async_check = false;
+var bt_index_drop_off_alphabet = /^[a-zA-Z0-9_$]*$/; //function name
+
+/*
+ initiate the beginning of the arrow function... use the unshifted bt_arrow parameter array for ordering
+*/
+
+function initiate_arrow(d, d_i) {
+  data = d;
+  data_index = d_i; //get rid of this and just set the bt_index to minice 1
+  bt_arrow_parameter_string = [];
+  bt_index = data_index - 1;
+  in_bt_quotation_string = [];
+  in_bt_string = false;
+  opening_bt_parentheses = 0;
+  closing_bt_parentheses = 0;
+  is_arrow = true;
+  bt_index_drop_off_found_first_character = false;
+  bt_index_drop_off_function_name = 0;
+  bt_index_drop_off_append_equals = false;
+  bt_af_is_async_check = false;
+  back_track_arrow(bt_index);
+}
 
 function back_track_arrow(bt_index) { 
 
@@ -56,7 +104,7 @@ function back_track_arrow(bt_index) {
   get_arrow_parameter_function_name(bt_index_drop_off_function_name);
   get_declaration_type(bt_index_drop_off_function_name);
   bt_arrow_parameter_string = bt_arrow_parameter_string.join();
-  return; 
+  return bt_arrow_parameter_string; //returns the beginning string of the function
  }
 
  /*
@@ -159,9 +207,11 @@ function is_async(bt_index_drop_off_function_name) {
 }
 
 /*
- get declaration type. backtrack until first letter and check for every type previous to
+ get declaration type. backtrack until first letter and check for every type previous to if - = let var const whatever...
 */
 
 function get_declaration_type(bt_index_drop_off_function_name) { 
 
 }
+
+module.exports = initiate_arrow;
