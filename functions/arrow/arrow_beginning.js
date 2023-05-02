39b65c12a782620eff1e64106eb951653596c638
Alexander Eatman <johnathanae1995@gmail.com>
2023-05-02 16:03:11 -0400

    update

--- functions/arrow/arrow_beginning.js
+++ functions/arrow/arrow_beginning.js
@@ -3,8 +3,10 @@
  recurses backwards the parameters of the arrow function and additional things
 */
 
+var update_function_and_update_data = require('../data');
+
 var data = '';
-var bt_arrow_parameter_string = [];
+var beginning_string = [];
 var bt_index = 0;
 var found_equals = false;
 var found_async = false;
@@ -16,8 +18,8 @@ var valid_parens = {};
 
 function initiate_arrow(data_index, boundries) {
  valid_parens = boundries;
- data = d;
- bt_arrow_parameter_string = [];
+ data = update_function_and_update_data.data;
+ beginning_string = [];
  bt_index = data_index - 1;
  found_equals = false;
  found_name = false;
@@ -25,34 +27,58 @@ function initiate_arrow(data_index, boundries) {
  found_async = false;
  opening_parameter_count = 0;
  closing_parameter_count = 0;
- try {
-  var a = is_parameter_set_or_is_name(bt_index);
-  if(a.is_parameter_set === true) { 
-   append_parameter_set(bt_index);
-  } else { 
-   append_name();
-  }
-  var b = append_possible_async(bt_index);
-  if(b.continue === false) {
-   return bt_arrow_parameter_string.join();
-  }
-  var c = append_possible_equals(bt_index);
-  if(c.continue === false) {
-   return bt_arrow_parameter_string.join();
-  }
-  append_name_and_possible_type(bt_index);
-  return bt_arrow_parameter_string.join();
- } catch(err) { 
-  console.log(err.message);
-  return '';
- }
+ is_parameter_set_or_is_name(bt_index);
 }
 
 function is_parameter_set_or_is_name(bt_index) { 
 
+ beginning_string.unshift(data.charAt(bt_index));
+
+ if(data.charAt(bt_index) === ')') { 
+  closing_parameter_count += 1; 
+  bt_index -= 1;
+  append_parameter_set(bt_index); 
+  return;
+ }
+
+ if(
+  data.charAt(bt_index) !== ')' && 
+  data.charAt(bt_index) !== ' ' && 
+  data.charAt(bt_index) !== '\n'
+  ) { 
+  bt_index -= 1;
+  append_name(bt_index); 
+  return;
+ }
+
+ bt_index -= 1; 
+ return is_parameter_set_or_is_name(bt_index);
+
 } 
 
 function append_name(bt_index) { 
+
+ beginning_string.unshift(data.charAt(bt_index));
+
+ if(data.charAt(bt_index) === '=') { 
+  found_equals = true;
+  bt_index -= 1;
+  append_name_and_possible_type(bt_index);
+  return;
+ }
+
+ if(
+  data.charAt(bt_index) === ' ' || 
+  data.charAt(bt_index) === '\n'
+ ) { 
+  found_equals = false;
+  bt_index -= 1;
+  append_possible_async(bt_index);
+  return;
+ }
+
+ bt_index -= 1; 
+ return append_name(bt_index);
 
 }
 
@@ -73,6 +99,20 @@ function append_parameter_set(bt_index) {
  ) { 
    opening_parameter_count += 1;
    if(opening_parameter_count === closing_parameter_count) { 
+    bt_index -= 1;
+    while(true) {
+     beginning_string.unshift(data.charAt(bt_index));
+     if(data.charAt(bt_index) === 'c') { 
+      bt_index -= 1;
+      append_async();
+      break;
+     } else if(data.charAt(bt_index) === '=') { 
+      bt_index -= 1;
+      append_name_and_possible_type();
+      break;
+     }
+     bt_index -= 1;
+    }
     return;
    }
  }
@@ -82,16 +122,41 @@ function append_parameter_set(bt_index) {
 
 }
 
-function append_possible_async(bt_index) { 
+function append_async(bt_index) { 
 
-}
-
-function append_possible_equals(bt_index) { 
+  if(
+   data.charAt(bt_index - 3) === 'a' &&
+   data.charAt(bt_index - 2) === 's' && 
+   data.charAt(bt_index - 1) === 'y' &&
+   data.charAt(bt_index) === 'n'
+  ) { 
+   beginning_string.unshift(data.charAt(bt_index));
+   beginning_string.unshift(data.charAt(bt_index - 1));
+   beginning_string.unshift(data.charAt(bt_index - 2));
+   beginning_string.unshift(data.charAt(bt_index - 3));
+   bt_index -= 4;
+   while(true) { 
+    beginning_string.unshift(data.charAt(bt_index));
+    if(data.charAt(bt_index) === '=') { 
+     bt_index -= 1;
+     append_name_and_possible_type(data.charAt(bt_index));
+     break;
+    }
+    bt_index -= 1;
+   }
+   return;
+  } else { 
+   throw new error(
+   'async was not found which is \n' +
+   'the only key word that can be allowed \n' +
+   'between the equals and the parameter set.'
+   )
+  }
 
 }
 
 function append_name_and_possible_type(bt_index) { 
-
+ //name then type
 }
 
 module.exports = initiate_arrow; 
