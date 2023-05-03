699c64b8c5c9c8e6f19c5aa03890a515657fe364
Alexander Eatman <johnathanae1995@gmail.com>
2023-05-02 20:06:13 -0400

    update

--- functions/arrow/arrow_beginning.js
+++ functions/arrow/arrow_beginning.js
@@ -14,6 +14,7 @@ var found_name = false;
 var end_name = false
 var opening_parameter_count = 0; 
 var closing_parameter_count = 0;
+var space_found = false;
 var valid_parens = {};
 
 function initiate_arrow(data_index, boundries) {
@@ -25,6 +26,7 @@ function initiate_arrow(data_index, boundries) {
  found_name = false;
  end_name = false;
  found_async = false;
+ space_found = false;
  opening_parameter_count = 0;
  closing_parameter_count = 0;
  is_parameter_set_or_is_name(bt_index);
@@ -61,7 +63,6 @@ function append_name(bt_index) {
  beginning_string.unshift(data.charAt(bt_index));
 
  if(data.charAt(bt_index) === '=') { 
-  found_equals = true;
   bt_index -= 1;
   append_name_and_possible_type(bt_index);
   return;
@@ -71,11 +72,11 @@ function append_name(bt_index) {
   data.charAt(bt_index) === ' ' || 
   data.charAt(bt_index) === '\n'
  ) { 
-  found_equals = false;
   bt_index -= 1;
   append_possible_async(bt_index);
   return;
  }
+
 
  bt_index -= 1; 
  return append_name(bt_index);
@@ -90,31 +91,19 @@ function append_parameter_set(bt_index) {
   data.charAt(bt_index) === ')' && 
   valid_parens[`${bt_index}-closing`] === true
  ) { 
-   closing_parameter_count += 1;
+  closing_parameter_count += 1;
  }
 
  if(
   data.charAt(bt_index) === '(' && 
   valid_parens[`${bt_index}-opening`] === true
  ) { 
-   opening_parameter_count += 1;
-   if(opening_parameter_count === closing_parameter_count) { 
-    bt_index -= 1;
-    while(true) {
-     beginning_string.unshift(data.charAt(bt_index));
-     if(data.charAt(bt_index) === 'c') { 
-      bt_index -= 1;
-      append_async();
-      break;
-     } else if(data.charAt(bt_index) === '=') { 
-      bt_index -= 1;
-      append_name_and_possible_type();
-      break;
-     }
-     bt_index -= 1;
-    }
-    return;
-   }
+  opening_parameter_count += 1;
+  if(opening_parameter_count === closing_parameter_count) { 
+   bt_index -= 1;
+   finish_parameter_set(bt_index)
+   return;
+  }
  }
 
  bt_index -= 1; 
@@ -122,41 +111,179 @@ function append_parameter_set(bt_index) {
 
 }
 
-function append_async(bt_index) { 
+function finish_parameter_set(bt_index) { 
+
+ while(true) {
 
   if(
-   data.charAt(bt_index - 3) === 'a' &&
-   data.charAt(bt_index - 2) === 's' && 
-   data.charAt(bt_index - 1) === 'y' &&
-   data.charAt(bt_index) === 'n'
+   data.charAt(bt_index) !== ' ' && 
+   data.charAt(bt_index) !== '\n' && 
+   data.charAt(bt_index) !== 'c' 
   ) { 
+   end = true; 
+   break;
+  } 
+
+  if(data.charAt(bt_index) === 'c') { 
+   append_async();
+   break;
+  } 
+
+  if(data.charAt(bt_index) === '=') { 
    beginning_string.unshift(data.charAt(bt_index));
-   beginning_string.unshift(data.charAt(bt_index - 1));
-   beginning_string.unshift(data.charAt(bt_index - 2));
-   beginning_string.unshift(data.charAt(bt_index - 3));
-   bt_index -= 4;
-   while(true) { 
-    beginning_string.unshift(data.charAt(bt_index));
-    if(data.charAt(bt_index) === '=') { 
-     bt_index -= 1;
-     append_name_and_possible_type(data.charAt(bt_index));
-     break;
-    }
-    bt_index -= 1;
-   }
-   return;
-  } else { 
-   throw new error(
-   'async was not found which is \n' +
-   'the only key word that can be allowed \n' +
-   'between the equals and the parameter set.'
-   )
+   bt_index -= 1;
+   append_name_and_possible_type();
+   break;
   }
+
+  beginning_string.unshift(data.charAt(bt_index));
+  bt_index -= 1;
+
+ }
+
+}
+
+function append_async(bt_index) { 
+
+ if(
+  data.charAt(bt_index - 4) === 'a' &&
+  data.charAt(bt_index - 3) === 's' && 
+  data.charAt(bt_index - 2) === 'y' &&
+  data.charAt(bt_index - 1) === 'n' &&
+  data.charAt(bt_index) === 'c'
+ ) { 
+  beginning_string.unshift(data.charAt(bt_index));
+  beginning_string.unshift(data.charAt(bt_index - 1));
+  beginning_string.unshift(data.charAt(bt_index - 2));
+  beginning_string.unshift(data.charAt(bt_index - 3));
+  beginning_string.unshift(data.charAt(bt_index - 4));
+  bt_index -= 5;
+  finish_async(bt_index);
+ }
+
+}
+
+function finish_async() { 
+
+ while(true) { 
+
+  if(
+   data.charAt(bt_index) !== ' ' && 
+   data.charAt(bt_index) !== '\n' && 
+   data.charAt(bt_index) !== '=' 
+  ) { 
+   end = true; 
+   break;
+  }
+
+  if(data.charAt(bt_index) === '=') { 
+   beginning_string.unshift(data.charAt(bt_index));
+   bt_index -= 1;
+   append_name_and_possible_type(bt_index);
+   break;
+  }
+
+  beginning_string.unshift(data.charAt(bt_index));
+  bt_index -= 1;
+
+ }
 
 }
 
 function append_name_and_possible_type(bt_index) { 
- //name then type
+
+ beginning_string.unshift(data.charAt(bt_index));
+
+ if(
+  space_found === false && 
+  data.charAt(bt_index) === ' ' || 
+  data.charAt(bt_index) === '\n'
+ ) { 
+  space_found === true
+ }
+
+ if(
+  space_found === true && 
+  data.charAt(bt_index) === ' ' || 
+  data.charAt(bt_index) === '\n'
+ ) { 
+  bt_index -= 1; 
+  get_type(bt_index)
+  return;
+ }
+
+ bt_index -= 1; 
+ return append_name_and_possible_type(bt_index);
+
+}
+
+function get_type(bt_index) { 
+
+ if(
+  data.charAt(bt_index) !== 't' &&
+  data.charAt(bt_index) !== 'r' && 
+  data.charAt(bt_index) !== ' ' && 
+  data.charAt(bt_index) !== '\n'
+ ) { 
+  return;
+ }
+
+ if(
+  data.charAt(bt_index) === 't' || 
+  data.charAt(bt_index) === 'r'
+ ) { 
+  append_type(bt_index);
+  return;
+ }
+
+ beginning_string.unshift(data.charAt(bt_index));
+
+ bt_index -= 1; 
+ return get_type(bt_index);
+ 
+}
+
+function append_type(bt_index) { 
+
+ if(  
+  (data.charAt(bt_index - 3) === ' ' || data.charAt(bt_index - 3) === '\n') &&
+  data.charAt(bt_index - 2) === 'l' && 
+  data.charAt(bt_index - 1) === 'e' &&
+  data.charAt(bt_index) === 't'
+ ) { 
+  beginning_string.unshift(data.charAt(bt_index));
+  beginning_string.unshift(data.charAt(bt_index - 1));
+  beginning_string.unshift(data.charAt(bt_index - 2));
+ }
+
+ if(  
+  (data.charAt(bt_index - 3) === ' ' || data.charAt(bt_index - 3) === '\n') &&
+  data.charAt(bt_index - 2) === 'v' && 
+  data.charAt(bt_index - 1) === 'a' &&
+  data.charAt(bt_index) === 'r'
+ ) { 
+  beginning_string.unshift(data.charAt(bt_index));
+  beginning_string.unshift(data.charAt(bt_index - 1));
+  beginning_string.unshift(data.charAt(bt_index - 2));
+ }
+
+ if(  
+  (data.charAt(bt_index - 5) === ' ' || data.charAt(bt_index - 5) === '\n') &&
+  data.charAt(bt_index - 4) === 'c' && 
+  data.charAt(bt_index -  3) === 'o' &&
+  data.charAt(bt_index - 2) === 'n' &&
+  data.charAt(bt_index - 1) === 's' &&
+  data.charAt(bt_index) === 't'
+ ) { 
+  beginning_string.unshift(data.charAt(bt_index));
+  beginning_string.unshift(data.charAt(bt_index - 1));
+  beginning_string.unshift(data.charAt(bt_index - 2));
+  beginning_string.unshift(data.charAt(bt_index - 3));
+  beginning_string.unshift(data.charAt(bt_index - 4));
+ }
+
+ return;
+
 }
 
 module.exports = initiate_arrow; 
