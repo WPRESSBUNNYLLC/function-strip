8c2115c3ed311c7ba50346b5e89ceec407aac75b
munetta <munetaalex@gmail.com>
2023-05-30 12:36:10 -0400

    update

--- tokens/numbers.js
+++ tokens/numbers.js
@@ -1,6 +1,6 @@
 
 let shared = require('../data');
-//binary, octal, hex, regular/exponent/decimal -- have to look ahead for some like e --- no, you can not find this on stack overflow... hand written pat. ya fuckin bum
+//binary, octal, hex, regular/exponent/decimal -- have to look ahead for some like e --- no, you can not find this on stack overflow... hand written...bitch
 const number_error_regular = /^0b([0-9]+)?$|^0o([0-9]+)?$|^0x([a-zA-Z0-9]+)?$|^([0-9]+)(\.)?([0-9]+)?([0-9]e[\-+][0-9]+)?$/;
 let is_hexa = false; 
 let is_binary = false; 
