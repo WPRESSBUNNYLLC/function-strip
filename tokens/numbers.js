325e59afba4b71c4568ffe32d135d7394e923032
munetta <munetaalex@gmail.com>
2023-05-30 15:40:41 -0400

    update

--- tokens/numbers.js
+++ tokens/numbers.js
@@ -1,14 +1,13 @@
 
 let shared = require('../data');
 
-const number_error_regular = /^0b([0-9]+)?$|^0o([0-9]+)?$|^0x([a-zA-Z0-9]+)?$|^([0-9]+)(\.)?([0-9]+)?([0-9]e[\-+][0-9]+)?$/;
 let has_decimal = false;
 let has_exponent = false;
 let numeric_type = '';
 
 function numbers() { 
  while(true) {
-  if(shared.get_current_token().test(number_error_regular) === false) { 
+  if(shared.get_current_token().test(/^0b([0-9]+)?$|^0o([0-9]+)?$|^0x([a-zA-Z0-9]+)?$|^([0-9]+)(\.)?([0-9]+)?([0-9]e[\-+][0-9]+)?$/) === false) {
    if(check_if_might_be_valid() === true) {
     continue;
    }
@@ -40,29 +39,34 @@ function numbers() {
 function check_numeric_type() { 
   if(shared.get_current_token().test(/0o([0-9]+)?$/) === true) { 
    numeric_type = 'octal;'
-  }
-  if(shared.get_current_token().test(/^0b([0-9]+)?$/) === true) { 
+  } else if(shared.get_current_token().test(/^0b([0-9]+)?$/) === true) { 
    numeric_type = 'binary;'
-  }
-  if(shared.get_current_token().test(/^([0-9]+)(\.)?([0-9]+)?([0-9]e[\-+][0-9]+)?$/) === true) { 
+  } else if(shared.get_current_token().test(/^([0-9]+)(\.)?([0-9]+)?([0-9]e[\-+][0-9]+)?$/) === true) { 
    numeric_type = 'regular;'
-  }
-  if(shared.get_current_token().test(/^0x([a-zA-Z0-9]+)?$/) === true) { 
+  } else if(shared.get_current_token().test(/^0x([a-zA-Z0-9]+)?$/) === true) { 
    numeric_type = 'hex;'
-  }    
+  } else {
+   console.log('error checking for numeric type')
+  }   
 }
 
 function check_if_might_be_valid() { 
  if(shared.get_current_token().slice(-1) === 'e' && numeric_type === 'regular') {
-  //if next token is either +,- and next is a number ...add to tokens, increase data index and continue
-  return true;
- } else {
-  return false;
+  if(
+   (shared.get_data().charAt(shared.get_data_index() + 1) === '+' || shared.get_data().charAt(shared.get_data_index() + 1) === '-') && 
+   shared.get_data().charAt(shared.get_data_index() + 2).test(/[0-9]/) === true
+  ) { 
+   shared.update_current_token(shared.get_data().charAt(shared.get_data_index() + 1))
+   shared.update_current_token(shared.get_data().charAt(shared.get_data_index() + 2))
+   shared.update_data_index(3);
+   return true;
+  }
  }
+ return false;
 }
 
 function combine_previous() { 
- //combine the beginning and ending of this and previous
+ 
 } 
 
 module.exports = numbers;
