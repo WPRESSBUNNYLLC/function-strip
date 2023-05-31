e7026842da68aa6c0af93a4b309804fb433a1cb0
munetta <munetaalex@gmail.com>
2023-05-31 11:46:11 -0400

    update

--- tokens/numbers.js
+++ tokens/numbers.js
@@ -1,37 +1,29 @@
 
 let shared = require('../data');
 
-let has_decimal = false;
-let has_exponent = false;
+let has_exponent = false; 
 let numeric_type = '';
 
 function numbers() { 
  while(true) {
-  if(shared.get_current_token().test(/^0b([0-9]+)?$|^0o([0-9]+)?$|^0x([a-zA-Z0-9]+)?$|^([0-9]+)(\.)?([0-9]+)?([0-9]e[\-+][0-9]+)?$/) === false) {
+  if(shared.get_current_token().test(/^0b([0-9]+)?$|^0o([0-9]+)?$|^0x([a-zA-Z0-9]+)?$|^([0-9]+)(\.)?([0-9]+)?([0-9]e[\-+]?[0-9]+)?$/) === false) {
    if(check_if_might_be_valid() === true) {
     continue;
    }
    shared.pop_current_token();
    shared.decrease_data_index_for_correct_data_index_and_line_number(1);
-   if(has_decimal === false) {
-    let tokens = shared.get_tokens()[shared.get_file_name()].tokens;
-    tokens[tokens.length - 1] === '.' && numeric_type === 'regular' ? combine_previous() : '';
-   }
    has_decimal = false;
    has_exponent = false;
    numeric_type = '';
    break;
   }
-  if(has_decimal === false && shared.get_data().charAt(shared.get_data_index()) === '.') {
-   has_decimal = true;
+  shared.update_current_token(shared.get_data().charAt(shared.get_data_index()));
+  if(numeric_type === '') {
+   check_numeric_type();
   }
   if(has_exponent === false && shared.get_data().charAt(shared.get_data_index()) === 'e') { 
    has_exponent = true;
   }
-  if(numeric_type === '') {
-   check_numeric_type();
-  }
-  shared.update_current_token(shared.get_data().charAt(shared.get_data_index()));
   shared.update_data_index(1);
  }
 }
@@ -41,12 +33,12 @@ function check_numeric_type() {
    numeric_type = 'octal;'
   } else if(shared.get_current_token().test(/^0b([0-9]+)?$/) === true) { 
    numeric_type = 'binary;'
-  } else if(shared.get_current_token().test(/^([0-9]+)(\.)?([0-9]+)?([0-9]e[\-+][0-9]+)?$/) === true) { 
+  } else if(shared.get_current_token().test(/^([0-9]+)(\.)?([0-9]+)?([0-9]e[\-+]?[0-9]+)?$/) === true) {
    numeric_type = 'regular;'
   } else if(shared.get_current_token().test(/^0x([a-zA-Z0-9]+)?$/) === true) { 
    numeric_type = 'hex;'
   } else {
-   console.log('error checking for numeric type')
+   numeric_type = 'regular';
   }   
 }
 
@@ -60,13 +52,13 @@ function check_if_might_be_valid() {
    shared.update_current_token(shared.get_data().charAt(shared.get_data_index() + 2))
    shared.update_data_index(3);
    return true;
+  } else if(shared.get_data().charAt(shared.get_data_index() + 1).test(/[0-9]/) === true) { 
+   shared.update_current_token(shared.get_data().charAt(shared.get_data_index() + 1))
+   shared.update_data_index(2);
+   return true;
   }
  }
  return false;
 }
-
-function combine_previous() { 
- 
-} 
 
 module.exports = numbers;
