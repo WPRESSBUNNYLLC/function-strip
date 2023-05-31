5c131c0aa57fdf2c0fd83a2634a0144503593138
munetta <munetaalex@gmail.com>
2023-05-31 18:14:12 -0400

    update

--- tokens/numbers.js
+++ tokens/numbers.js
@@ -1,43 +1,29 @@
 
 let shared = require('../data');
-let numeric_type = '';
+let checked_valid = false;
 
 function numbers() { 
  while(true) {
   if(shared.get_current_token().test(/^0b([10]+)$|^0o([0-7]+)$|^0x([a-fA-f0-9]+)$|^(\.[0-9]{1,}|[0]\.?[0-9]{0,}|[1-9]{1}[0-9]{0,}\.?[0-9]{0,})(e[\-+][0-9]+)?$/) === false) {
-   if(check_if_might_be_valid() === true) {
+   if(checked_valid === false && check_if_might_be_valid() === true) {
     continue;
    }
    shared.pop_current_token();
    shared.decrease_data_index_for_correct_data_index_and_line_number(1);
-   numeric_type = '';
+   checked_valid = false;
    break;
   }
   shared.update_current_token(shared.get_data().charAt(shared.get_data_index()));
-  if(numeric_type === '') {
-   check_numeric_type();
-  }
   shared.update_data_index(1);
  }
 }
 
-function check_numeric_type() { 
-  if(shared.get_current_token().test(/0o([0-7]+)$/) === true) { 
-   numeric_type = 'octal;'
-  } else if(shared.get_current_token().test(/^0b([10]+)$/) === true) { 
-   numeric_type = 'binary;'
-  } else if(shared.get_current_token().test(/^(\.[0-9]{1,}|[0]\.?[0-9]{0,}|[1-9]{1}[0-9]{0,}\.?[0-9]{0,})(e[\-+][0-9]+)?$/) === true) {
-   numeric_type = 'regular;'
-  } else if(shared.get_current_token().test(/^0x([a-fA-f0-9]+)$/) === true) { 
-   numeric_type = 'hex;'
-  } else {
-   numeric_type = 'regular';
-  }   
-}
-
 function check_if_might_be_valid() { 
 
- if(shared.get_current_token().slice(-1) === 'e' && numeric_type === 'regular') {
+ checked_valid = true;
+ let last = shared.get_current_token().slice(-1);
+
+ if(last === 'e') {
   if(
    (shared.get_data().charAt(shared.get_data_index() + 1) === '+' || shared.get_data().charAt(shared.get_data_index() + 1) === '-') && 
    shared.get_data().charAt(shared.get_data_index() + 2).test(/[0-9]/) === true
@@ -49,7 +35,7 @@ function check_if_might_be_valid() {
   }
  }
 
- if(shared.get_current_token().slice(-1) === 'o' && numeric_type === 'octal') {
+ if(last === 'o') {
   if(shared.get_data().charAt(shared.get_data_index() + 1).test(/[0-7]/)) { 
    shared.update_current_token(shared.get_data().charAt(shared.get_data_index() + 1))
    shared.update_data_index(2);
@@ -57,7 +43,7 @@ function check_if_might_be_valid() {
   }
  }
 
- if(shared.get_current_token().slice(-1) === 'x' && numeric_type === 'hex') {
+ if(last === 'x') {
   if(shared.get_data().charAt(shared.get_data_index() + 1).test(/[a-fA-f0-9]/)) { 
    shared.update_current_token(shared.get_data().charAt(shared.get_data_index() + 1))
    shared.update_data_index(2);
@@ -65,7 +51,7 @@ function check_if_might_be_valid() {
   }
  }
 
- if(shared.get_current_token().slice(-1) === 'b' && numeric_type === 'binary') {
+ if(last === 'b') {
   if(shared.get_data().charAt(shared.get_data_index() + 1).test(/[10]/)) { 
    shared.update_current_token(shared.get_data().charAt(shared.get_data_index() + 1))
    shared.update_data_index(2);
